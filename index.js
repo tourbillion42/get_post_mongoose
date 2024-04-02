d3bc03e4be660a5247654903c813be9c0686ec28
HwangRangyu <tourbillion42@gmail.com>
2024-04-02 23:03:18 +0900

    import testModel.js , testRoutes.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from "express"
 import mongoose from "mongoose"
 import { POST, mongodbURL } from "./config.js"
+import { TEST } from "./model/testModel.js"
+import testRoutes from "./routes/testRoutes.js"
 
 const app = express() 
 
 app.use(express.json())
 
 app.get('/', (req, res, next) => {
-    
+    console.log('Welcome to my world ~ !!');
 })
+
+app.use('/test', testRoutes)
 
 mongoose
     .connect(mongodbURL)
